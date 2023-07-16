c3d3163bd703bd224df83fa3f2739fa07f3a402d
KleemoffDeveloper <siliskleemoff@pursuit.org>
2023-07-16 08:40:43 -0400

    backend lookin pretty good

---
+++ app.js
@@ -0,0 +1,63 @@
+const express = require('express')
+const app = express()
+const transactions = require("./models/transactions");
+
+// INDEX
+app.get("/", (req, res) => {
+    res.json(transactions);
+  });
+  
+  // SHOW
+  app.get("/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const transaction = transactions.find(
+      (transaction) => transaction.id === id
+    );
+  
+    if (transaction) {
+      res.json(transaction);
+    } else {
+      res.status(404).json({ error: "Not Found" });
+    }
+  });
+  
+  // CREATE
+  app.post("/", (req, res) => {
+    const newTransaction = req.body;
+    newTransaction.id = transactions.length + 1; // Generate new ID
+    transactions.push(newTransaction);
+    res.status(201).json(newTransaction); // Return created transaction with ID
+  });
+  
+  // DELETE
+  app.delete("/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const index = transactions.findIndex(
+      (transaction) => transaction.id === id
+    );
+  
+    if (index !== -1) {
+      const deletedTransaction = transactions.splice(index, 1);
+      res.status(200).json(deletedTransaction);
+    } else {
+      res.status(404).json({ error: "Not Found" });
+    }
+  });
+  
+  // UPDATE
+  app.put("/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const updatedTransaction = req.body;
+    const index = transactions.findIndex(
+      (transaction) => transaction.id === id
+    );
+  
+    if (index !== -1) {
+        transactions[index] = updatedTransaction;
+      res.status(200).json(updatedTransaction);
+    } else {
+      res.status(404).json({ error: "Not Found" });
+    }
+  });
+
+module.exports = app
